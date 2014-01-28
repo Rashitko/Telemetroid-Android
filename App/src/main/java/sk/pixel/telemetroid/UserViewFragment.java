669cc2b8e921304eb9554e9caf10f54cd10e096d
Rashitko <michal.raska@gmail.com>
2014-01-28 14:57:39 +0100

    Vytvoreny fragment

---
+++ App/src/main/java/sk/pixel/telemetroid/UserViewFragment.java
@@ -0,0 +1,14 @@
+package sk.pixel.telemetroid;
+
+import android.support.v4.app.Fragment;
+
+public class UserViewFragment extends Fragment {
+
+    public interface UserViewEventsListener {
+
+    }
+
+    public UserViewFragment() {
+    }
+
+}
